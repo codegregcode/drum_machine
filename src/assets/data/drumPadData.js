61a66a1386f6dcd45b7121e86ec3133d7529fcb9
Greg Cain <codegregcode@gmail.com>
2024-10-05 09:59:38 +0100

    creates data and font assets

---
+++ src/assets/data/drumPadData.js
@@ -0,0 +1,40 @@
+const drumPad = [
+  {
+    char: 'Q',
+    src: 'https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-3.mp3',
+  },
+  {
+    char: 'W',
+    src: 'https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-2.mp3',
+  },
+  {
+    char: 'E',
+    src: 'https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-1.mp3',
+  },
+  {
+    char: 'A',
+    src: 'https://cdn.freecodecamp.org/testable-projects-fcc/audio/Dsc_Oh.mp3',
+  },
+  {
+    char: 'S',
+    src: 'https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-6.mp3',
+  },
+  {
+    char: 'D',
+    src: 'https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-4_1.mp3',
+  },
+  {
+    char: 'Z',
+    src: 'https://cdn.freecodecamp.org/testable-projects-fcc/audio/Cev_H2.mp3',
+  },
+  {
+    char: 'X',
+    src: 'https://cdn.freecodecamp.org/testable-projects-fcc/audio/RP4_KICK_1.mp3',
+  },
+  {
+    char: 'C',
+    src: 'https://cdn.freecodecamp.org/testable-projects-fcc/audio/Kick_n_Hat.mp3',
+  },
+];
+
+export default drumPad;
